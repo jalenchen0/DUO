9c8889220db6e8d64635646b4ebd43ec85fe7a5f
Ricky Lin <rlin61@stuy.edu>
2026-01-13 07:23:12 -0500

    added basic templates and working flask for a working app

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,11 @@
 from flask import Flask, render_template, request, flash, url_for, redirect, session
 import sqlite3   #enable control of an sqlite database
 import csv       #facilitate CSV I/O
-import db
+#import db
 import json
 from urllib.request import Request, urlopen
 import pprint
 import os
-import api
 import re
 # Initialize databases
 
@@ -15,13 +14,27 @@ app = Flask(__name__)
 app.secret_key = os.urandom(24)
 
 @app.route("/", methods=['GET', 'POST'])
-def homepage():
+def default():
     return render_template("homepage.html")
 @app.route("/login", methods=['GET', 'POST'])
+def login():
+    return render_template("login.html");
 @app.route("/logout", methods=['GET', 'POST'])
+def logout():
+    return render_template("logout.html");
 @app.route("/home", methods=['GET', 'POST'])
 def homepage():
     return render_template("homepage.html")
 @app.route("/create", methods=['GET', 'POST'])
+def create():
+    return render_template("create.html");
 @app.route("/profile", methods=['GET', 'POST'])
+def profile():
+    return render_template("profile.html");
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    return render_template("register.html");
 
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
